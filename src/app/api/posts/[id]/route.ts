f384f05a73412c83fec0f0bec97994a1e2447393
LeadTool User <rayparker6515@gmail.com>
2025-12-19 12:47:47 -0500

    blog subsite uploaded third 2025.12.19

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { createServerSupabaseClient } from '@/lib/supabase'
-import { calculateReadingTime } from '@/lib/utils'
+import { calculateReadingTime, slugify } from '@/lib/utils'
 
 export async function GET(
   request: NextRequest,
