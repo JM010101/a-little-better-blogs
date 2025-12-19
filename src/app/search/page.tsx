894c69ca5fd8f8152940a9dbeb443a12f2a11bef
LeadTool User <rayparker6515@gmail.com>
2025-12-19 14:18:34 -0500

    blog subsite uploaded sixth 2025.12.19

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -25,7 +25,7 @@ async function SearchResults({ query }: { query: string }) {
 
   return (
     <>
-      {posts.length > 0 ? (
+      {posts && posts.length > 0 ? (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
           {posts.map((post: any) => (
             <PostCard key={post.id} post={post} />
